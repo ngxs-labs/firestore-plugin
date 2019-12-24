7251d877aeadefea8c3c891b7b55e7653a9f289c
Joaquin Cid <joaqcid@gmail.com>
2019-12-24 15:30:14 -0300

    fix: list component test

--- integration/app/components/list/list.component.spec.ts
+++ integration/app/components/list/list.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { Store, NgxsModule } from '@ngxs/store';
+import { NgxsFirestoreModule } from 'src';
+import { Subject } from 'rxjs';
 
 describe('ListComponent', () => {
     let component: ListComponent;
     let fixture: ComponentFixture<ListComponent>;
+    let storeMock;
 
     beforeEach(async(() => {
+
+        storeMock = jest.fn().mockImplementation(() => ({
+            select: jest.fn().mockReturnValue(new Subject())
+        }));
+
         TestBed.configureTestingModule({
-            declarations: [ListComponent]
+            imports: [
+                NgxsModule.forRoot(),
+                NgxsFirestoreModule.forRoot()
+            ],
+            declarations: [
+                ListComponent
+            ],
+            providers: [{ provide: Store, useValue: storeMock() }]
         }).compileComponents();
     }));
 
