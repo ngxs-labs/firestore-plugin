ad4d9fcecdde415afeda2d6914e63ec1f231e083
Joaquin Cid <joaqcid@gmail.com>
2020-02-25 09:15:13 -0300

    test: test args action, payload and lifecylce

--- src/lib/ngxs-firestore-connect.spec.ts
+++ src/lib/ngxs-firestore-connect.spec.ts
@@ -1,17 +1,26 @@
 import { TestBed, fakeAsync } from '@angular/core/testing';
 import { NgxsFirestoreConnect } from './ngxs-firestore-connect.service';
-import { Store, NgxsModule, State, NgxsOnInit, Action, StateContext } from '@ngxs/store';
+import { Store, NgxsModule, State, NgxsOnInit, Action, StateContext, getActionTypeFromInstance } from '@ngxs/store';
 import { NgxsFirestoreModule } from './ngxs-firestore.module';
-import { from } from 'rxjs';
-import { Emitted } from './types';
-import { StreamEmitted } from './action-decorator-helpers';
+import { BehaviorSubject } from 'rxjs';
+import { Emitted, Connected, Disconnected } from './types';
+import { StreamEmitted, StreamConnected, StreamDisconnected } from './action-decorator-helpers';
 
 describe('NgxsFirestoreConnect', () => {
     let store: Store;
+    let events: ('emmited' | 'connected' | 'disconnected')[];
 
-    const mockFirestoreStream = from([1]);
+    const mockFirestoreStream = jest.fn();
+    const emittedFn = jest.fn();
+    const connectedFn = jest.fn();
+    const disconnectedFn = jest.fn();
     class TestAction {
         static type = 'TEST ACTION';
+    }
+
+    class TestActionWithPayload {
+        static type = 'TEST ACTION WITH PAYLOAD';
+        constructor(public payload: string) {}
     }
 
     @State({
@@ -22,13 +31,51 @@ describe('NgxsFirestoreConnect', () => {
 
         ngxsOnInit() {
             this.ngxsFirestoreConnect.connect(TestAction, {
-                to: () => mockFirestoreStream
+                to: mockFirestoreStream
+            });
+
+            this.ngxsFirestoreConnect.connect(TestActionWithPayload, {
+                to: mockFirestoreStream
             });
         }
 
         @Action(StreamEmitted(TestAction))
-        testAction(ctx: StateContext<any>, { payload }: Emitted<TestAction, number>) {
-            ctx.patchState({ number: payload });
+        testActionEmitted(ctx: StateContext<any>, { action, payload }: Emitted<TestAction, number>) {
+            emittedFn(action);
+            events.push('emmited');
+        }
+
+        @Action(StreamConnected(TestAction))
+        testActionConnected(ctx: StateContext<any>, { action }: Connected<TestAction>) {
+            connectedFn(action);
+            events.push('connected');
+        }
+
+        @Action(StreamDisconnected(TestAction))
+        testActionDisconnected(ctx: StateContext<any>, { action }: Disconnected<TestAction>) {
+            disconnectedFn(action);
+            events.push('disconnected');
+        }
+
+        @Action(StreamEmitted(TestActionWithPayload))
+        testActionWithPayloadEmitted(
+            ctx: StateContext<any>,
+            { action, payload }: Emitted<TestActionWithPayload, number>
+        ) {
+            emittedFn(action);
+            events.push('emmited');
+        }
+
+        @Action(StreamConnected(TestActionWithPayload))
+        testActionWithPayloadConnected(ctx: StateContext<any>, { action }: Connected<TestActionWithPayload>) {
+            connectedFn(action);
+            events.push('connected');
+        }
+
+        @Action(StreamDisconnected(TestActionWithPayload))
+        testActionWithPayloadDisconnected(ctx: StateContext<any>, { action }: Disconnected<TestActionWithPayload>) {
+            disconnectedFn(action);
+            events.push('disconnected');
         }
     }
 
@@ -37,6 +84,11 @@ describe('NgxsFirestoreConnect', () => {
             imports: [NgxsModule.forRoot([TestState]), NgxsFirestoreModule.forRoot()]
         });
         store = TestBed.get(Store);
+        events = [];
+        mockFirestoreStream.mockImplementation(() => new BehaviorSubject(1).asObservable());
+        connectedFn.mockReset();
+        emittedFn.mockReset();
+        disconnectedFn.mockReset();
     });
 
     test('should be provided by module', () => {
@@ -44,8 +96,73 @@ describe('NgxsFirestoreConnect', () => {
         expect(TestBed.get(NgxsFirestoreConnect)).toBeTruthy();
     });
 
-    test('', fakeAsync(() => {
-        store.dispatch(TestAction);
-        expect(store.selectSnapshot(TestState).number).toBe(1);
-    }));
+    describe('Action', () => {
+        describe('Dispatched as Type', () => {
+            test('should connect and emit', fakeAsync(() => {
+                store.dispatch(TestAction);
+                expect(events).toEqual(['connected', 'emmited']);
+            }));
+
+            test('should receive action on connected', fakeAsync(() => {
+                connectedFn.mockImplementation((action) => {
+                    expect(getActionTypeFromInstance(action)).toBe(TestAction.type);
+                });
+                store.dispatch(TestAction);
+            }));
+
+            test('should receive action on emitted', fakeAsync(() => {
+                emittedFn.mockImplementation((action) => {
+                    expect(getActionTypeFromInstance(action)).toBe(TestAction.type);
+                });
+                store.dispatch(TestAction);
+            }));
+        });
+        describe('Dispatched as instance', () => {
+            test('should connect and emit', fakeAsync(() => {
+                store.dispatch(TestAction);
+                expect(events).toEqual(['connected', 'emmited']);
+            }));
+
+            test('should receive action on connected', fakeAsync(() => {
+                connectedFn.mockImplementation((action) => {
+                    expect(getActionTypeFromInstance(action)).toBe(TestAction.type);
+                });
+                store.dispatch(TestAction);
+            }));
+
+            test('should receive action on emitted', fakeAsync(() => {
+                emittedFn.mockImplementation((action) => {
+                    expect(getActionTypeFromInstance(action)).toBe(TestAction.type);
+                });
+                store.dispatch(TestAction);
+            }));
+        });
+    });
+
+    describe('Action With Payload', () => {
+        const payload = 'test-payload';
+
+        test('should connect and emit', fakeAsync(() => {
+            store.dispatch(new TestActionWithPayload(payload));
+            expect(events).toEqual(['connected', 'emmited']);
+        }));
+
+        test('should receive action on connected', fakeAsync(() => {
+            connectedFn.mockImplementation((action) => {
+                expect(getActionTypeFromInstance(action)).toBe(TestActionWithPayload.type);
+            });
+            store.dispatch(new TestActionWithPayload(payload));
+        }));
+
+        test('should receive action and payload on emitted', fakeAsync(() => {
+            emittedFn.mockImplementation((action) => {
+                expect(getActionTypeFromInstance(action)).toBe(TestActionWithPayload.type);
+            });
+
+            emittedFn.mockImplementation((action) => {
+                expect(action.payload).toBe(payload);
+            });
+            store.dispatch(new TestActionWithPayload(payload));
+        }));
+    });
 });
