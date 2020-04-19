a234fa8dcc3accd40704947fd739d452086f84b9
Joaquin Cid <joaqcid@gmail.com>
2020-04-19 10:43:09 -0300

    feat: public ngxs-firestore state

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,6 +1,7 @@
 export * from './lib/ngxs-firestore.module';
 export * from './lib/ngxs-firestore.service';
 export * from './lib/ngxs-firestore-connect.service';
+export * from './lib/ngxs-firestore.state';
 export * from './lib/action-decorator-helpers';
 export * from './lib/actions';
 export * from './lib/types';
