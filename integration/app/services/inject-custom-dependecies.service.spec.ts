cdb3d6b76cae3114bc5fced022843915acf8c402
Joaquin Cid <joaqcid@gmail.com>
2020-12-19 19:02:41 -0300

    test: test inject adapter on NgxsFirestoreService

---
+++ integration/app/services/inject-custom-dependecies.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { NgxsFirestoreModule } from '@ngxs-labs/firestore-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { CustomDependency, InjectCustomDependenciesService } from './inject-custom-dependecies.service';
+
+describe('InjectCustomDependenciesService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([]), NgxsFirestoreModule.forRoot()],
+      providers: [
+        { provide: CustomDependency, useClass: CustomDependency },
+        { provide: AngularFirestore, useValue: jest.fn() }
+      ]
+    })
+  );
+
+  it('should be created', () => {
+    const service: InjectCustomDependenciesService = TestBed.get(InjectCustomDependenciesService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should inject a custom dep', () => {
+    const service: InjectCustomDependenciesService = TestBed.get(InjectCustomDependenciesService);
+    expect(service.customeDependency.works()).toBeTruthy();
+  });
+});
