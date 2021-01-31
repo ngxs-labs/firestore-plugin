3d999d9f4c06798496dc70fe41d32019f083d949
Joaquin Cid <joaqcid@gmail.com>
2021-01-31 18:44:45 -0300

    test: fix

--- integration/app/states/classifications/classifications.state.spec.ts
+++ integration/app/states/classifications/classifications.state.spec.ts
@@ -5,6 +5,7 @@ import { NgxsFirestoreModule } from '@ngxs-labs/firestore-plugin';
 import { BehaviorSubject } from 'rxjs';
 import { ClassificationsActions } from './classifications.actions';
 import { Classification } from 'integration/app/models/classification';
+import { ClassificationsFirestore } from 'integration/app/services/classifications.firestore';
 
 describe('Classifications State', () => {
   let store: Store;
@@ -13,21 +14,22 @@ describe('Classifications State', () => {
 
   beforeEach(async(() => {
     mockClassificationsFS = jest.fn(() => ({
-      collection$: mockCollection$
+      collection$: mockCollection$,
+      setRaceId: jest.fn()
     }));
 
     mockCollection$ = jest.fn();
 
     TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([ClassificationsState]), NgxsFirestoreModule.forRoot()],
-      providers: [{ provide: ClassificationsState, useValue: mockClassificationsFS() }]
+      providers: [{ provide: ClassificationsFirestore, useValue: mockClassificationsFS() }]
     }).compileComponents();
     store = TestBed.get(Store);
   }));
 
   it('should getall classifications', () => {
     mockCollection$.mockReturnValue(new BehaviorSubject([{ id: 'a' }]));
-    store.dispatch(new ClassificationsActions.GetAll());
+    store.dispatch(new ClassificationsActions.GetAll('id'));
     expect(store.selectSnapshot(ClassificationsState.classifications)).toEqual([{ id: 'a' } as Classification]);
   });
 });
