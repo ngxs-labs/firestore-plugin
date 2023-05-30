f012e2c1ca2aeb50f2f03f80d0569b985f0212e5
Joaquin Cid <joaqcid@gmail.com>
2023-05-30 17:14:53 -0300

    chore: no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,7 +70,7 @@ module.exports = {
         "@typescript-eslint/naming-convention": "error",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-empty-interface": "error",
-        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-explicit-any": "error",
         "@typescript-eslint/no-inferrable-types": "off",
         "@typescript-eslint/no-misused-new": "error",
         "@typescript-eslint/no-non-null-assertion": "error",
