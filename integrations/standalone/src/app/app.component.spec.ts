2b7710b184caf14b1a52cc06ca51f0b99647d541
joaq cid <joaqcid@gmail.com>
2024-04-15 10:36:32 -0300

    chore: fix test

--- integrations/standalone/src/app/app.component.spec.ts
+++ integrations/standalone/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { importProvidersFrom } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent],
+      providers: [importProvidersFrom(NgxsModule.forRoot([]))]
     }).compileComponents();
   });
 
@@ -17,13 +20,13 @@ describe('AppComponent', () => {
   it(`should have the 'standalone' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.name).toEqual('standalone');
+    expect(app.name).toEqual('Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, standalone');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello from Angular!');
   });
 });
